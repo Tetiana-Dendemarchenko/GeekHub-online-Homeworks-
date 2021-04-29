5c8a3793717e234a9186940e7fea4e55fc798cc9
Tetiana Dendemarchenko <69472920+Tetiana-Dendemarchenko@users.noreply.github.com>
2021-04-29 06:25:50 +0300

    Update tagging.js

--- Homework #6/tagging.js
+++ Homework #6/tagging.js
@@ -1,58 +1,95 @@
-const dragElements = document.querySelectorAll(".tagging-element");
-const elName = document.querySelectorAll(".tagging-element-name");
-const closingEl = document.querySelectorAll(".delete");
-const droppableArea = document.querySelector(".tagging-wrapper");
+const draggableElement = document.querySelectorAll ( '.tagging-element' );
+const elementName = document.querySelectorAll ( '.tagging-element-name' );
+const closingElement = document.querySelectorAll ( ".delete" );
+const droppableArea = document.querySelector ( '.tagging-wrapper' );
+let draggableItem = {};
 
-elName.forEach((element, indOfEl) => element.addEventListener("click", () => {
-    closingEl.forEach((closeEl, indOfClosingEl) => {
-        if (indOfEl === indOfClosingEl) {
-            closeEl.style.visibility = "visible";
-        } else {
-            closeEl.style.visibility = "hidden";
-        }
-    })
-}));
+elementName.forEach ( (element, elementIndex) =>
+    element.addEventListener ( 'click', () => {
 
-closingEl.forEach((closeEl) => {
-    closeEl.addEventListener("click", (event) => {
-        event.target.parentNode.remove();
-    })
-});
+        closingElement.forEach ( (closingEl, closingElementIndex) => {
+            ( elementIndex === closingElementIndex ) ? closingEl.style.visibility = 'visible' :
+                closingEl.style.visibility = 'hidden';
+        } );
 
-for(let i = 0; i < dragElements.length; i++){
-    dragElement(dragElements[i]);
+        draggableElement.forEach ( (draggableElement, draggableElementIndex) => {
+            ( draggableElementIndex === elementIndex ) ? draggableElement.classList.add ( 'draggingElement' ) :
+                draggableElement.classList.remove ( 'draggingElement' );
+        } );
+
+    } ) );
+
+closingElement.forEach ( (closingEl) => {
+    closingEl.addEventListener ( 'click', (event) => {
+        event.target.parentNode.remove ();
+    } );
+} );
+
+function formMovableElement() {
+    return draggableItem.elem;
 }
 
-function dragElement(elmnt) {
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id)) {
-        document.getElementById(elmnt.id).onmousedown = dragMouseDown;
-    } else {
-        elmnt.onmousedown = dragMouseDown;
-    }
-    function dragMouseDown(e) {
-        e = e || window.event;
-        pos3 = parseInt(e.clientX);
-        pos4 = parseInt(e.clientY);
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-        return false;
-    }
+function positionChangeOnMouseDown({ pageX, pageY, target, which }) {
+    const draggedElement = target.closest ( '.draggingElement' );
+    if ( which !== 1 ) return;
+    if ( ! draggedElement ) return;
 
-    function elementDrag(e) {
-        e = e || window.event;
-        pos1 = pos3 - parseInt(e.clientX);
-        pos2 = pos4 - parseInt(e.clientY);
-        pos3 = parseInt(e.clientX);
-        pos4 = parseInt(e.clientY);
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        console.log(elmnt.offsetTop)
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
+    draggableItem.elem = draggedElement;
+    draggableItem.downX = pageX;
+    draggableItem.downY = pageY;
 }
+
+function positionChangeOnMouseMove(event) {
+    if ( ! draggableItem.elem ) return;
+    if ( ! draggableItem.movableElement ) {
+
+        draggableItem.movableElement = formMovableElement ( event );
+
+        let positionLeft = draggableItem.movableElement.getBoundingClientRect ().left + pageXOffset;
+        let positionTop = draggableItem.movableElement.getBoundingClientRect ().top + pageYOffset;
+
+        draggableItem.shiftX = draggableItem.downX - positionLeft;
+        draggableItem.shiftY = draggableItem.downY - positionTop;
+
+        startDraggingAction ();
+    }
+
+    let topPosition;
+    topPosition = droppableArea.getBoundingClientRect ().top - 1;
+    let bottomPosition;
+    bottomPosition = droppableArea.getBoundingClientRect ().bottom - 1;
+    let rightPosition;
+    rightPosition = droppableArea.getBoundingClientRect ().right - 23;
+    const { left: leftPosition } = droppableArea.getBoundingClientRect ();
+    let closingBtn;
+    closingBtn = draggableItem.elem.querySelector ( '.delete' );
+    const { shiftX } = draggableItem;
+    let posX = event.pageX - shiftX;
+    let posY;
+    posY = event.pageY - draggableItem.shiftY;
+    posX = Math.min ( posX, rightPosition - draggableItem.movableElement.clientWidth );
+    posX = Math.max ( posX, leftPosition );
+    posY = Math.min ( posY, bottomPosition - draggableItem.movableElement.clientHeight );
+    posY = Math.max ( posY, topPosition );
+
+    draggableItem.movableElement.style.left = posX + 'px';
+    draggableItem.movableElement.style.top = posY + 'px';
+
+    ( parseInt ( draggableItem.movableElement.style.left ) >
+        ( rightPosition - draggableItem.movableElement.clientWidth - closingBtn.clientWidth ) ) ?
+        closingBtn.classList.add ( 'move-left' ) :
+        closingBtn.classList.remove ( 'move-left' );
+}
+
+function positionChangeOnMouseUp() {
+    draggableItem = {};
+}
+
+function startDraggingAction() {
+    document.body.appendChild ( draggableItem.elem );
+}
+
+document.onmousemove = positionChangeOnMouseMove;
+document.onmouseup = positionChangeOnMouseUp;
+document.onmousedown = positionChangeOnMouseDown;
 
